2e981e6558ff2fbcf8aaf74f930436ba321c32d2
Ishan211 <ishan.leung211@gmail.com>
2024-09-16 00:43:51 -0400

    commented file for clarity

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 #include <cstdlib>
 #include <ctime>
 
-using namespace std;
-
 // Function prototypes
 void displayBoard(char board[3][3]); // disp board
 bool checkWin(char board[3][3], char player); // check win
 bool checkDraw(char board[3][3]); // check draw
-void playerMove(char board[3][3], vector<int>& x, vector<int>& y, vector<int>& x_p, vector<int>& y_p); // player move
-void cpuMove(char board[3][3], vector<int>& x, vector<int>& y, vector<int>& x_c, vector<int>& y_c); // cpu move
+void playerMove(char board[3][3], std::vector<int>& x, std::vector<int>& y, std::vector<int>& x_p, std::vector<int>& y_p); // player move
+void cpuMove(char board[3][3], std::vector<int>& x, std::vector<int>& y, std::vector<int>& x_c, std::vector<int>& y_c); // cpu move
 
 int main() {
     // Init board
@@ -21,12 +19,12 @@ int main() {
     };
 
     // vec for pos board
-    vector<int> x = {0, 0, 0, 1, 1, 1, 2, 2, 2};
-    vector<int> y = {0, 1, 2, 0, 1, 2, 0, 1, 2};
+    std::vector<int> x = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+    std::vector<int> y = {0, 1, 2, 0, 1, 2, 0, 1, 2};
 
     // vec for player and cpu move
-    vector<int> x_p, y_p;    
-    vector<int> x_c, y_c;
+    std::vector<int> x_p, y_p;    
+    std::vector<int> x_c, y_c;
 
     char currentPlayer = 'X';
     bool gameWon = false; // bool for win
@@ -41,10 +39,11 @@ int main() {
         if (currentPlayer == 'X') {
             playerMove(board, x, y, x_p, y_p); // player move
             gameWon = checkWin(board, currentPlayer); // check if player won
-            currentPlayer = 'O'; // cpu turn
+            currentPlayer = '0'; // cpu turn
         } else {
+            std::cout << "Player's turn" << std::endl;
             cpuMove(board, x, y, x_c, y_c); // cpu move
-            gameWon = checkWin(board, 'O'); // Check if cpu won
+            gameWon = checkWin(board, '0'); // Check if cpu won
             currentPlayer = 'X'; // player turn
         }
 
@@ -55,13 +54,13 @@ int main() {
 
     // give game(res)
     if (gameWon) {
-        if (currentPlayer == 'O') { // If the currentPlayer 'O' player 'X' won
-            cout << "Player wins!" << endl;
+        if (currentPlayer == '0') { // If the currentPlayer 'O' player 'X' won
+            std::cout << "Player wins!" << std::endl;
         } else { // otherwise cpu won
-            cout << "CPU wins!" << endl;
+            std::cout << "CPU wins!" << std::endl;
         }
     } else if (gameDraw) {
-        cout << "It's a draw!" << endl; // If gameDraw is true then its a draw
+        std::cout << "It's a draw!" << std::endl; // If gameDraw is true then its a draw
     }
 
     return 0; // Exit with 0
@@ -69,28 +68,28 @@ int main() {
 
 // disp current board
 void displayBoard(char board[3][3]) {
-    cout << "-------------" << endl; //board frame
+    std::cout << "-------------" << std::endl; //board frame
     for (int i = 0; i < 3; i++) {
-        cout << "| ";
+        std::cout << "| ";
         for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << " | "; // disp each board(cell)
+            std::cout << board[i][j] << " | "; // disp each board(cell)
         }
-        cout << endl << "-------------" << endl; //separate rows
+        std::cout << std::endl << "-------------" << std::endl; //separate rows
     }
 }
 
 // player moves
-void playerMove(char board[3][3], vector<int>& x, vector<int>& y, vector<int>& x_p, vector<int>& y_p) {
+void playerMove(char board[3][3], std::vector<int>& x, std::vector<int>& y, std::vector<int>& x_p, std::vector<int>& y_p) {
     int choice;
-    cout << "Enter the number of the cell where you'd like to place X: ";
-    cin >> choice;
+    std::cout << "Enter the number of the cell where you'd like to place X: ";
+    std::cin >> choice;
 
     // find board coords (xi,yi) from user inp
     int xi = (choice - 1) / 3; // row
     int yi = (choice - 1) % 3; // column
 
     // if spot is free
-    if (board[xi][yi] != 'X' && board[xi][yi] != 'O') {
+    if (board[xi][yi] != 'X' && board[xi][yi] != '0') {
         board[xi][yi] = 'X'; // put x on board
         x_p.push_back(xi); // store move x
         y_p.push_back(yi); // store move y
@@ -105,20 +104,22 @@ void playerMove(char board[3][3], vector<int>& x, vector<int>& y, vector<int>& x
         }
     } else {
         // If the move is invalid ask for new inp
-        cout << "Invalid move! Try again." << endl;
+        std::cout << "Invalid move! Try again." << std::endl;
         playerMove(board, x, y, x_p, y_p); // recurse until inp = valid move 
     }
 }
 
+
+
 // cpu moves
-void cpuMove(char board[3][3], vector<int>& x, vector<int>& y, vector<int>& x_c, vector<int>& y_c) {
+void cpuMove(char board[3][3], std::vector<int>& x, std::vector<int>& y, std::vector<int>& x_c, std::vector<int>& y_c) {
     if (x.empty()) return; // do nothing if nothing can be done
 
     int randIndex = rand() % x.size(); // rand find move
     int xi = x[randIndex]; // get x of move
     int yi = y[randIndex]; // get y of move
 
-    board[xi][yi] = 'O'; // put o on board
+    board[xi][yi] = '0'; // put o on board
     x_c.push_back(xi); // store move x
     y_c.push_back(yi); // store move y
 
@@ -149,7 +150,7 @@ bool checkDraw(char board[3][3]) {
     // loop thru all cells
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (board[i][j] != 'X' && board[i][j] != 'O') {
+            if (board[i][j] != 'X' && board[i][j] != '0') {
                 return false; // if empty cell, != draw
             }
         }
